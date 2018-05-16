70cf06654f6d16010bfa46754ea1f536945875c6
Fernando Mayorga <mayorgarojasgamedeveloper>
2018-05-16 18:02:35 -0500

    Se agrego la seccion de promociones para el publico

--- js/views/promociones.js
+++ js/views/promociones.js
@@ -1,1 +1,14 @@
-console.log('Promociones');
+$('document').ready(function() {
+  $.get( "https://donleonapi.herokuapp.com/package", function( data ) {
+    var html = ``;
+    $.each(data, function(index, value) {
+      html += `  <div class="jumbotron">`;
+      html += `<span class="badge badge-secondary">Nuevo</span>`;
+      html += `<h1 class="display-4">${value.name}</h1>`;
+      html += `<hr class="my-4">`;
+      html += `<p class="lead">${value.description}</p>`;
+      html += `</div>`;
+    });
+    $('#promociones').append(html);
+  });
+});
